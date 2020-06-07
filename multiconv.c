e47dbcee47155d470235daa7466be659d96f82c2
Arkaeriit <maxbouillot@gmail.com>
2020-06-07 17:19:59 +0200

    Number are showed in binairy

--- multiconv.c
+++ multiconv.c
@@ -3,17 +3,30 @@
 #include <inttypes.h>
 #include <string.h>
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+    (byte & 0x80 ? '1' : '0'), \
+    (byte & 0x40 ? '1' : '0'), \
+    (byte & 0x20 ? '1' : '0'), \
+    (byte & 0x10 ? '1' : '0'), \
+    (byte & 0x08 ? '1' : '0'), \
+    (byte & 0x04 ? '1' : '0'), \
+    (byte & 0x02 ? '1' : '0'), \
+    (byte & 0x01 ? '1' : '0') 
+
 void prompt(char* str){
     printf(">");
     fgets(str, 1000, stdin);
 }
 
 void detail(uint64_t num){
+    char* numCH = &num;
     printf("ASCII : %c\n"
            "décimal : %i\n"
            "héxadécimal : %x\n"
+           "binaire : " BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN "\n"
            "----------------------------\n"
-           ,(char) num, num, num);
+           ,(char) num, num, num, BYTE_TO_BINARY(numCH[7]), BYTE_TO_BINARY(numCH[6]), BYTE_TO_BINARY(numCH[5]), BYTE_TO_BINARY(numCH[4]), BYTE_TO_BINARY(numCH[3]), BYTE_TO_BINARY(numCH[2]), BYTE_TO_BINARY(numCH[1]), BYTE_TO_BINARY(numCH[0]));
 }
 
 void mainLoop(){
